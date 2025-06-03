2cc90bd2c063a3c42b95b68b8dbe0d51bddfdb70
sz0p <32867417+sz0p@users.noreply.github.com>
2025-06-03 10:01:21 +0200

    Update ServersController.php

--- app/Http/Controllers/Admin/ServersController.php
+++ app/Http/Controllers/Admin/ServersController.php
@@ -70,7 +70,7 @@ class ServersController extends Controller
     public function setDetails(Request $request, Server $server): RedirectResponse
     {
         $this->detailsModificationService->handle($server, $request->only([
-            'owner_id', 'external_id', 'name', 'description',
+            'owner_id', 'external_id', 'name', 'description', 'external_ip',
         ]));
 
         $this->alert->success(trans('admin/server.alerts.details_updated'))->flash();
